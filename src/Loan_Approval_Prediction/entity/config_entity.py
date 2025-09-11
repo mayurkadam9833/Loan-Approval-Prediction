ae6d8a31517dfa292e31e205d73a7b33607a3884
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-09-12 00:14:23 +0530

    updated entity for data ingestion config

--- src/Loan_Approval_Prediction/entity/config_entity.py
+++ src/Loan_Approval_Prediction/entity/config_entity.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from dataclasses import dataclass 
 
 # Configuration dataclass for data ingestion process (directories, file paths, and source URL)
-dataclass(frozen=True)
+@dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_url: str 
